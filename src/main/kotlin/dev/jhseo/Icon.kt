0fb76df8f7a0fb891222da18990c560d47392589
Janghyub Seo <seojanghyeob@gmail.com>
2021-05-11 12:13:17 +0900

    Full implementation

--- src/main/kotlin/dev/jhseo/Icon.kt
+++ src/main/kotlin/dev/jhseo/Icon.kt
@@ -6,17 +6,31 @@ object Icon {
     private val iconThemes: MutableMap<String, MutableList<File>> = mutableMapOf()
     private val individualIcons: MutableMap<String, File> = mutableMapOf()
 
+    private val home: File
+    private val xdgDataHome: File
+    private val xdgDataDirs: List<File>
+    private val xdgConfigHome: File
+    private val xdgConfigDirs: List<File>
+
     init {
-        if(System.getenv("HOME").isNullOrBlank())
+        if (System.getenv("HOME").isNullOrBlank())
             throw Exception("Environment variable HOME is null or blank.")
 
-        val home = File(System.getenv("HOME"))
-        val xdgDataHome = if (System.getenv("XDG_DATA_HOME").isNullOrBlank()) {
+        home = File(System.getenv("HOME"))
+        xdgDataHome = if (System.getenv("XDG_DATA_HOME").isNullOrBlank()) {
             home.resolve(".local/share")
         } else {
             File(System.getenv("XDG_DATA_HOME"))
         }
-        val xdgDataDirs: List<File> = (System.getenv("XDG_DATA_DIRS") ?: "/usr/local/share/:/usr/share/")
+        xdgDataDirs = (System.getenv("XDG_DATA_DIRS") ?: "/usr/local/share/:/usr/share/")
+            .split(":")
+            .map { File(it) }
+        xdgConfigHome = if (System.getenv("XDG_CONFIG_HOME").isNullOrBlank()) {
+            home.resolve(".config")
+        } else {
+            File(System.getenv("XDG_CONFIG_HOME"))
+        }
+        xdgConfigDirs = (System.getenv("XDG_CONFIG_DIRS") ?: "/etc/xdg")
             .split(":")
             .map { File(it) }
 
@@ -30,38 +44,99 @@ object Icon {
             File("/usr/share/pixmaps"),
         ).forEach {
             if (it.exists())
-                it.listFiles()?.forEach { it ->
-                    if (it.exists() && it.isDirectory)
-                        if (iconThemes[it.name] != null)
-                            iconThemes[it.name]?.add(it)
+                it.listFiles()?.forEach { file ->
+                    if (file.exists() && file.isDirectory)
+                        if (iconThemes[file.name] != null)
+                            iconThemes[file.name]?.add(file)
                         else
-                            iconThemes[it.name] = mutableListOf(it)
-                    // Icons are searched in order from override to overridden,
+                            iconThemes[file.name] = mutableListOf(file)
+                    // Icons are searched in order of from override to overridden,
                     // so latter files shouldn't replace the former files.
-                    if (it.exists() && it.isFile && !individualIcons.containsKey(it.nameWithoutExtension))
-                        individualIcons[it.nameWithoutExtension] = it
+                    if (file.exists() && file.isFile && !individualIcons.containsKey(file.nameWithoutExtension))
+                        individualIcons[file.nameWithoutExtension] = file
                 }
         }
-
-        // If no icons were found during the process,
-        // we would expect that the code is not running on a freedesktop environment.
-        if(iconThemes.isEmpty() && individualIcons.isEmpty())
-            throw Exception("KIcon is not running on a freedesktop environment.")
     }
 
-    private fun currentIconTheme(): MutableList<File> {
-        TODO()
+    private fun parseIni(file: File, field: String, flagName: String?): String? {
+        if (!file.exists()) return null
+
+        var flag = flagName.isNullOrEmpty()
+        var returnVal: String? = null
+        file.forEachLine {
+            if (it.contains("[$flagName]")) flag = true
+            if (flag && it.contains(field)) {
+                returnVal = it.split("=").last().replace("\\s".toRegex(), "")
+                return@forEachLine
+            }
+        }
+        return returnVal
     }
 
-    private fun currentIconThemeWithDepends(): MutableList<File> {
-        TODO()
+    private fun gtkThemeName(): String? {
+        val gtk4 = xdgConfigHome.resolve("gtk-4.0").resolve("settings.ini")
+        val gtk3 = xdgConfigHome.resolve("gtk-3.0").resolve("settings.ini")
+
+        return if (gtk4.exists()) {
+            parseIni(gtk4, "gtk-icon-theme-name", "Settings")
+        } else if (gtk3.exists()) {
+            parseIni(gtk3, "gtk-icon-theme-name", "Settings")
+        } else {
+            null
+        }
+    }
+
+    private fun plasmaThemeName(): String? =
+        parseIni(xdgConfigHome.resolve("kdeglobals"), "Theme", "Icons")
+
+    fun themeName(): String? =
+        gtkThemeName() ?: plasmaThemeName()
+
+    private fun themeNameWithDepends(name: String): List<String>? {
+        val themeFiles = iconThemes[name]
+        if (themeFiles.isNullOrEmpty()) return null
+
+        val themeNames: MutableList<String> = mutableListOf(name)
+        themeFiles.forEach {
+            val index = it.resolve("index.theme")
+            parseIni(index, "Inherits", "Icon Theme")?.also {
+                it.split(",").forEach {
+                    if (!themeNames.contains(it)) themeNames.add(it)
+                }
+            }
+        }
+
+        return themeNames
+    }
+
+    private fun fetch(name: String, theme: File): File? {
+        val candidates: MutableList<File> = mutableListOf()
+        theme.listFiles()?.forEach {
+            if (it.isFile && it.nameWithoutExtension == name) {
+                candidates.add(it)
+            } else if (it.isDirectory) {
+                fetch(name, it)?.let { candidates.add(it) }
+            }
+        }
+        candidates.sortBy { it.length() }
+        return if (candidates.isEmpty()) null else candidates[0]
     }
 
     fun fetch(name: String, themeName: String): File? {
-        TODO()
+        iconThemes[themeName]?.forEach {
+            fetch(name, it)?.also { return it }
+        }
+        return null
     }
 
     fun fetch(name: String): File? {
-        TODO()
+        themeName()?.also {
+            themeNameWithDepends(it)?.forEach {
+                fetch(name, it)?.also {
+                    if (it.exists()) return it
+                }
+            }
+        }
+        return individualIcons[name]
     }
 }
