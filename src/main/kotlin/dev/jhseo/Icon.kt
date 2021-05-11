656693c7a06b7033a091498af434f9ac37d0ccfa
DisLogicator <59501233+DisLogicator@users.noreply.github.com>
2021-05-11 14:31:33 +0900

    Update Icon.kt

--- src/main/kotlin/dev/jhseo/Icon.kt
+++ src/main/kotlin/dev/jhseo/Icon.kt
@@ -5,7 +5,6 @@ import java.io.File
 object Icon {
     private val iconThemes: MutableMap<String, MutableList<File>> = mutableMapOf()
     private val individualIcons: MutableMap<String, File> = mutableMapOf()
-
     private val home: File
     private val xdgDataHome: File
     private val xdgDataDirs: List<File>
@@ -15,7 +14,6 @@ object Icon {
     init {
         if (System.getenv("HOME").isNullOrBlank())
             throw Exception("Environment variable HOME is null or blank.")
-
         home = File(System.getenv("HOME"))
         xdgDataHome = if (System.getenv("XDG_DATA_HOME").isNullOrBlank()) {
             home.resolve(".local/share")
@@ -33,7 +31,6 @@ object Icon {
         xdgConfigDirs = (System.getenv("XDG_CONFIG_DIRS") ?: "/etc/xdg")
             .split(":")
             .map { File(it) }
-
         // By default, apps should look in $HOME/.icons (for backwards compatibility),
         // in $XDG_DATA_DIRS/icons and in /usr/share/pixmaps (in that order).
         // - freedesktop.org icon theme specifications version 0.13
@@ -57,10 +54,8 @@ object Icon {
                 }
         }
     }
-
     private fun parseIni(file: File, field: String, flagName: String?): String? {
         if (!file.exists()) return null
-
         var flag = flagName.isNullOrEmpty()
         var returnVal: String? = null
         file.forEachLine {
@@ -72,30 +67,27 @@ object Icon {
         }
         return returnVal
     }
-
     private fun gtkThemeName(): String? {
         val gtk4 = xdgConfigHome.resolve("gtk-4.0").resolve("settings.ini")
         val gtk3 = xdgConfigHome.resolve("gtk-3.0").resolve("settings.ini")
-
+        val gtk2 = xdgConfigHome.resolve("gtk-2.0").resolve("settings.ini")
         return if (gtk4.exists()) {
             parseIni(gtk4, "gtk-icon-theme-name", "Settings")
         } else if (gtk3.exists()) {
             parseIni(gtk3, "gtk-icon-theme-name", "Settings")
+        } else if (gtk2.exists()) {
+            parseIni(gtk2, "gtk-icon-theme-name", "Settings")
         } else {
             null
         }
     }
-
     private fun plasmaThemeName(): String? =
         parseIni(xdgConfigHome.resolve("kdeglobals"), "Theme", "Icons")
-
     fun themeName(): String? =
         gtkThemeName() ?: plasmaThemeName()
-
     private fun themeNameWithDepends(name: String): List<String>? {
         val themeFiles = iconThemes[name]
         if (themeFiles.isNullOrEmpty()) return null
-
         val themeNames: MutableList<String> = mutableListOf(name)
         themeFiles.forEach {
             val index = it.resolve("index.theme")
@@ -105,10 +97,8 @@ object Icon {
                 }
             }
         }
-
         return themeNames
     }
-
     private fun fetch(name: String, theme: File): File? {
         val candidates: MutableList<File> = mutableListOf()
         theme.listFiles()?.forEach {
@@ -121,14 +111,12 @@ object Icon {
         candidates.sortBy { it.length() }
         return if (candidates.isEmpty()) null else candidates[0]
     }
-
     fun fetch(name: String, themeName: String): File? {
         iconThemes[themeName]?.forEach {
             fetch(name, it)?.also { return it }
         }
         return null
     }
-
     fun fetch(name: String): File? {
         themeName()?.also {
             themeNameWithDepends(it)?.forEach {
