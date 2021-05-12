0bfe20722ef2c2c9d6c8d078e36160bd07488665
Janghyub Seo <seojanghyeob@gmail.com>
2021-05-12 15:52:58 +0900

    add logo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.0"
+    `maven-publish`
 }
 
 group = "dev.jhseo"
@@ -7,4 +8,17 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("Ikon")
+                description.set("Kotlin/JVM library that fetches icons on a freedesktop environment.")
+                url.set("https://github.com/Jhyub/Ikon")
+
+            }
+        }
+    }
 }
