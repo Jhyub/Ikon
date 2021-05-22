338cf5c654e4aecdfe088f3af8ae0dfce41cb576
Janghyub Seo <seojanghyeob@gmail.com>
2021-05-22 10:57:52 +0900

    To be continued..

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.5.0"
     `maven-publish`
+    signing
 }
 
 group = "dev.jhseo"
@@ -8,17 +9,4 @@ version = "1.0"
 
 repositories {
     mavenCentral()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            pom {
-                name.set("Ikon")
-                description.set("Kotlin/JVM library that fetches icons on a freedesktop environment.")
-                url.set("https://github.com/Jhyub/Ikon")
-
-            }
-        }
-    }
 }
