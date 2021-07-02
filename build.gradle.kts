5552b4ca9c17d682282b493a99d6a5518149822f
Janghyub Seo <seojanghyeob@gmail.com>
2021-07-02 18:55:57 +0900

    Add gradle nexus publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,30 @@
 plugins {
     kotlin("jvm") version "1.5.10"
     maven
-    signing
+    `java-library`
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "dev.jhseo"
 version = "1.0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
 
 repositories {
     mavenCentral()
